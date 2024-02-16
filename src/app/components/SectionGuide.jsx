e4a1ff74f204d76d0d28168bfebf8b59922baf55
emmanueljose.molina <emmanueljose.molina@salud.gob.sv>
2024-02-16 08:51:00 -0600

    Finished section guide

--- src/app/components/SectionGuide.jsx
+++ src/app/components/SectionGuide.jsx
@@ -1,26 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function SectionGuide() {
   return (
-    <section className='py-5 flex flex-col items-center justify-center'>
-        <h2 className='text-3xl mb-16'>Guía de compra</h2>
+    <section className='py-5 flex flex-col items-center justify-center mt-5 mx-20'>
+        <h2 className='text-6xl mb-16 font-bold text-center'>Guía de compra</h2>
         
-        <div className='flex w-full mx-20'>
-            <div className='flex items-center justify-center p-10 border-r w-1/4 h-24'>
-                Diseñar
-            </div>
+        <div className='grid grid-cols-12 w-full'>
+            <Link href="/configurator" className='col-span-6 md:col-span-3 flex flex-col items-center justify-center gap-y-3 h-24 font-bold min-h-32 hover:underline underline-offset-4 border-r'>
+                <Image width={60} height={60} src="/tool.svg" alt="Icono de imagen diseñar"/>
+                <h5>Diseñar</h5>
+            </Link>
 
-            <div className='flex items-center justify-center p-10 border-r w-1/4 h-24'>
-                Ver inventario
-            </div>
+            <Link href="/configurator" className='col-span-6 md:col-span-3 flex flex-col items-center justify-center gap-y-3 h-24 font-bold min-h-32 hover:underline underline-offset-4 md:border-r'>
+                <Image width={60} height={60} src="/search.svg" alt="Icono de imagen diseñar"/>
+                <h5>Ver inventario</h5>
+            </Link>
 
-            <div className='flex items-center justify-center p-10 border-r w-1/4 h-24'>
-                Ofertas locales
-            </div>
+            <Link href="/configurator" className='col-span-6 md:col-span-3 flex flex-col items-center justify-center gap-y-3 h-24 font-bold min-h-32 hover:underline underline-offset-4 border-r border-t md:border-t-0'>
+                <Image width={60} height={60} src="/money.svg" alt="Icono de imagen diseñar"/>
+                <h5>Ofertas locales</h5>
+            </Link>
 
-            <div className='flex items-center justify-center p-10  w-1/4 h-24'>
-                Concesionarios
-            </div>
+            <Link href="/configurator" className='col-span-6 md:col-span-3 flex flex-col items-center justify-center gap-y-3 h-24 font-bold min-h-32 hover:underline underline-offset-4 border-t md:border-t-0'>
+                <Image width={60} height={60} src="/find.svg" alt="Icono de imagen diseñar"/>
+                <h5> Concesionarios</h5>
+            </Link>
         </div>
     </section>
   )
