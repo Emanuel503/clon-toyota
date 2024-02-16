6e77d076d7a9dba20966f53b11ccf41a3122cf05
emmanueljose.molina <emmanueljose.molina@salud.gob.sv>
2024-02-16 10:03:19 -0600

    fix error section video

--- src/app/components/SectionVideo.jsx
+++ src/app/components/SectionVideo.jsx
@@ -12,7 +12,7 @@ export default function SectionVideo() {
             <Link href="/tacoma" className='text-white bg-black rounded-3xl px-7 py-2 text-center'>Ver más</Link>
         </div>
 
-        <iframe className='w-2/3 h-full' src="https://www.youtube.com/embed/Ju0DdwUybXk?si=FWWRjOPzPDlKj25H" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe className='w-2/3 h-full' src="https://www.youtube.com/embed/Ju0DdwUybXk?si=FWWRjOPzPDlKj25H" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
     </section>
   )
 }
