c376f36a5d74253a9a6f638856c13089eb9f6632
emmanueljose.molina <emmanueljose.molina@salud.gob.sv>
2024-02-16 08:56:14 -0600

    Add alts to images

--- src/app/components/SectionGuide.jsx
+++ src/app/components/SectionGuide.jsx
@@ -14,18 +14,18 @@ export default function SectionGuide() {
             </Link>
 
             <Link href="/configurator" className='col-span-6 md:col-span-3 flex flex-col items-center justify-center gap-y-3 h-24 font-bold min-h-32 hover:underline underline-offset-4 md:border-r'>
-                <Image width={60} height={60} src="/search.svg" alt="Icono de imagen diseñar"/>
+                <Image width={60} height={60} src="/search.svg" alt="Icono de ver inventario"/>
                 <h5>Ver inventario</h5>
             </Link>
 
             <Link href="/configurator" className='col-span-6 md:col-span-3 flex flex-col items-center justify-center gap-y-3 h-24 font-bold min-h-32 hover:underline underline-offset-4 border-r border-t md:border-t-0'>
-                <Image width={60} height={60} src="/money.svg" alt="Icono de imagen diseñar"/>
+                <Image width={60} height={60} src="/money.svg" alt="Icono de ofertas locales"/>
                 <h5>Ofertas locales</h5>
             </Link>
 
             <Link href="/configurator" className='col-span-6 md:col-span-3 flex flex-col items-center justify-center gap-y-3 h-24 font-bold min-h-32 hover:underline underline-offset-4 border-t md:border-t-0'>
-                <Image width={60} height={60} src="/find.svg" alt="Icono de imagen diseñar"/>
-                <h5> Concesionarios</h5>
+                <Image width={60} height={60} src="/find.svg" alt="Icono de consesionarios"/>
+                <h5>Concesionarios</h5>
             </Link>
         </div>
     </section>
