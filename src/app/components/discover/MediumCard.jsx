833428fcd7c163ef7bf13a6b0df5bda068b6a3f7
Emanuel Molina <emanueljosemolina@gmail.com>
2024-03-20 09:22:53 -0600

    Add transition section explorer

--- src/app/components/discover/MediumCard.jsx
+++ src/app/components/discover/MediumCard.jsx
@@ -1,26 +1,25 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function MediumCard({link = "/", image = "", imageDescription = "", title = "", subtitle = "", description = "", button = "Ver más" }) {
+export default function MediumCard({ link = "/", image = "", imageDescription = "", title = "", subtitle = "", description = "", button = "Ver más" }) {
+    const [isHovered, setIsHovered] = useState(false);
 
-    const hadlerDiscoverHover = (e, type) => {
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    }
 
-        const element = e.target.querySelector('p');
-
-        if (element) {
-            element.classList.toggle('block', type);
-            element.classList.toggle('hidden', !type);
-        }
+    const handleMouseLeave = () => {
+        setIsHovered(false);
     }
 
     return (
         <div className='col-span-12 md:col-span-6 rounded-lg relative'>
-            <Link href={link} onMouseEnter={(e) => hadlerDiscoverHover(e, true)} onMouseLeave={(e) => hadlerDiscoverHover(e, false)} className="w-full h-full absolute bg-gradient-to-t from-[#12120f] rounded-lg">
+            <Link href={link} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="w-full h-full absolute bg-gradient-to-t from-[#12120f] rounded-lg">
                 <div className='absolute bottom-0 text-white mx-5 mb-8'>
                     <h5 className='text-xs'>{title}</h5>
                     <h3 className='text-2xl font-bold mb-5'>{subtitle}</h3>
-                    <p className='text-sm mb-5 hidden'>{description}</p>
+                    <p className={`text-sm mb-5 transition-all duration-500 h-0 ${isHovered ? 'opacity-100 h-fit' : 'opacity-0'}`}>{description}</p>
                 
                     <button className='bg-white py-3 px-8 text-black text-sm rounded-3xl font-semibold hover:bg-gray-400 transition-all duration-500' href={link}>
                         {button}
