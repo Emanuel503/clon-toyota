cac52dd0aa24a40bd662f1fd16fcb683f66d6348
Emanuel Molina <emanueljosemolina@gmail.com>
2024-04-03 08:14:09 -0600

    finished menu guia de compra

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -469,6 +469,47 @@ export default function Nav() {
             </div>
          </div>
         }
+
+        {
+         menu == 2 &&
+         <div className="bg-white py-8 border-t rounded-b-lg w-full lg:w-11/12 xl:w-4/6 right-0  xl:mr-20 absolute z-20 grid grid-cols-12">
+            <div className="col-span-6 border-gray-300 border-r p-5 grid grid-cols-12 gap-y-12 gap-x-8">
+              <div className="col-span-6">
+                <Link className="font-semibold flex items-center gap-2 text-sm hover:underline" href="/">Diseña y cotiza <MdOutlineKeyboardArrowRight className='text-xl'/> </Link>
+                <p className="text-gray-500 text-sm">Diseña tu toyota</p>
+              </div>
+              <div className="col-span-6">
+                <Link className="font-semibold flex items-center gap-2 text-sm hover:underline" href="/">Diseña y cotiza <MdOutlineKeyboardArrowRight className='text-xl'/> </Link>
+                <p className="text-gray-500 text-sm">Ver modelos usados</p>
+              </div>
+              <div className="col-span-6">
+                <Link className="font-semibold flex items-center gap-2 text-sm hover:underline" href="/">Diseña y cotiza <MdOutlineKeyboardArrowRight className='text-xl'/> </Link>
+                <p className="text-gray-500 text-sm">Encuentra tu auto</p>
+              </div>
+              <div className="col-span-6">
+                <Link className="font-semibold flex items-center gap-2 text-sm hover:underline" href="/">Diseña y cotiza <MdOutlineKeyboardArrowRight className='text-xl'/> </Link>
+                <p className="text-gray-500 text-sm">Personaliza tu toyota</p>
+              </div>
+            </div>
+
+            <div className="col-span-3 border-gray-300 border-r p-5">
+              <Link href="/" className="text-xs text-gray-700 block mb-4 hover:underline">Buscar concesionarios</Link>
+              <Link href="/" className="text-xs text-gray-700 block mb-4 hover:underline">Ver folletos</Link>
+              <Link href="/" className="text-xs text-gray-700 block mb-4 hover:underline">Comparar Modelos</Link>
+              <Link href="/" className="text-xs text-gray-700 block mb-4 hover:underline">Pedir cotización</Link>
+              <Link href="/" className="text-xs text-gray-700 block mb-4 hover:underline">Toyota Financial Services</Link>
+              <Link href="/" className="text-xs text-gray-700 block mb-0 hover:underline">Southeast Toyota Finance</Link>
+            </div>
+
+            <div className="col-span-3 p-5">
+              <Link href="/" className="text-xs text-gray-700 block mb-4 hover:underline">Calcula tu pago</Link>
+              <Link href="/" className="text-xs text-gray-700 block mb-4 hover:underline">Qué se ajusta a mi presupuesto</Link>
+              <Link href="/" className="text-xs text-gray-700 block mb-4 hover:underline">Valor de intercambio Kelly Blue Book</Link>
+              <Link href="/" className="text-xs text-gray-700 block mb-4 hover:underline">Ofertas locales</Link>
+              <Link href="/" className="text-xs text-gray-700 block mb-0 hover:underline">Comprar partes y accesorios</Link>
+            </div>
+         </div>
+        }
     </nav>
   )
 }
